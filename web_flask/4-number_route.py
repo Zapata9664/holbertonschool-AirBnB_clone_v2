71bd333cd08bcd253192bade27b92cef4a2e0efb
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-14 12:26:41 -0500

    Display int

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_HBNB():
+    """Display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def print_HBNB():
+    """Display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def text(text):
+    """Display c + text"""
+    text = text.replace("_", " ")
+    return ("C {}".format(text))
+
+
+@app.route("/python", defaults={"text": "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    """Display Python + text"""
+    text = text.replace("_", " ")
+    return ("Python {}".format(text))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def display_int(n):
+    """Display a int number"""
+    return ("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(port=5000, host="0.0.0.0")
