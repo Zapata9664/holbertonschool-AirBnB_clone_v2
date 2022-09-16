7cb68b2af052e2f53f07dac61d0320205586dbfe
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-15 23:36:04 -0500

    Fix pycodestyle

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,50 +1,54 @@
 #!/usr/bin/python3
-from models.base_model import Base
-from models.user import User
-from models.state import State
-from models.place import Place
-from models.amenity import Amenity
-from models.review import Review
-from models.city import City
+"""New DBStorage"""
 from sqlalchemy import (create_engine)
 from os import getenv
-from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
 
 
-class DBStorage():
-    """Class DBStorage"""
+class DBStorage:
+    """Database storage"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """Public instance methods"""
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
-                                      .format(getenv("HBNB_MYSQL_USER"),
-                                              getenv("HBNB_MYSQL_PWD"),
-                                              getenv("HBNB_MYSQL_HOST"),
-                                              getenv("HBNB_MYSQL_DB")),
+        """Initialize DBStorage instance"""
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB")),
                                       pool_pre_ping=True)
+
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """The following values must be retrieved via environment variables"""
-        classes = [User, State, Place, Amenity, Review, City]
-        my_dict = {}
+        """query on the current database session (self.__session) all
+        objects depending of the class name"""
+        classes = [User, State, Place, City, Amenity, Review]
+        result = {}
         if cls in classes:
             search = self.__session.query(cls)
             for object in search:
                 key = "{}.{}".format(type(object).__name__,
                                      object.id)
-                my_dict[key] = object
+                result[key] = object
         elif cls is None:
             for class_ in classes:
                 search = self.__session.query(class_)
                 for object in search:
                     key = "{}.{}".format(type(object).__name__,
                                          object.id)
-                    my_dict[key] = object
-        return my_dict
+                    result[key] = object
+        return result
 
     def new(self, obj):
         """Add the object to the current database session"""
@@ -55,15 +59,12 @@ class DBStorage():
         self.__session.commit()
 
     def delete(self, obj=None):
-        """
-        Delete from the current database
-        session obj if not None
-        """
+        """Delete from the current database session obj if not None"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the databas"""
+        """Create all tables in the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
@@ -71,5 +72,6 @@ class DBStorage():
         self.__session = Session()
 
     def close(self):
-        """Close the session"""
-        self.__session.remove()
+        """Call remove() method on the private session attribute
+        (self.__session) or close() on the class Session"""
+        self.__session.close()
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-""""""
-from flask import Flask
-from flask import render_template
+""" 7-states_list.py"""
+from flask import Flask, render_template
 from models import storage
-
+from models import *
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_list():
-    states = storages.all("State")
-    return render_templates("7-states_list.html", states=states)
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """states_lis Function that displays a HTML"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """"""
+def teardown_db(exception):
+    """Function that removes the current SQLAlchemy Session"""
     storage.close()
 
 
-
-if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
